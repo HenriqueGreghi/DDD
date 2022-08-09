5162fa06d667f3799e8b54f9ccaa4d10bda33fec
HenriqueGreghi <92762708+HenriqueGreghi@users.noreply.github.com>
2022-08-09 17:14:54 -0300

    Add files via upload

---
+++ DDD/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Funcao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Digite o numero do DDD pretendido: ");
+            int x = int.Parse(Console.ReadLine());
+            string ddd = Endereco(x);
+
+            Console.WriteLine(ddd);
+        }
+
+        private static string Endereco(int a)
+        {
+            string ddd;
+            switch (a)
+            {
+                case 61:
+                    ddd = "Brasilia";
+                    break;
+                case 71:
+                    ddd = "Salvador";
+                    break;
+                case 11:
+                    ddd = "Sao Paulo";
+                    break;
+                case 21:
+                    ddd = "Rio de Janeiro";
+                    break;
+                case 32:
+                    ddd = "Juiz de Fora";
+                    break;
+                case 19:
+                    ddd = "Campinas";
+                    break;
+                case 27:
+                    ddd = "Vitoria";
+                    break;
+                case 31:
+                    ddd = "Belo Horizonte";
+                    break;
+                default:
+                    ddd = "DDD nao cadastrado";
+                    break;
+
+            }
+            return ddd; ;
+        }
+
+
+
+    }
+}
